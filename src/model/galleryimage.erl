535376361b0a7285a118f9bb416ddbc25247cf96
Anders Pearson <anders@columbia.edu>
2012-02-12 21:37:14 -0500

    create hook for setting ordinality

--- src/model/galleryimage.erl
+++ src/model/galleryimage.erl
@@ -3,3 +3,10 @@
 
 -belongs_to(gallery).
 -belongs_to(image).
+
+before_create() ->
+    ModifiedRecord = set(ordinality,
+			 boss_db:count(galleryimage, 
+				       [gallery_id = GalleryId]) + 1),
+    {ok, ModifiedRecord}.
+    
