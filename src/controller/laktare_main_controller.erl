9b464c37e8b97d3747a14da159219d495786ddee
Anders Pearson <anders@columbia.edu>
2012-02-19 16:05:58 -0500

    working towards image upload handling

--- src/controller/laktare_main_controller.erl
+++ src/controller/laktare_main_controller.erl
@@ -5,3 +5,16 @@ index('GET',[]) ->
     Galleries = boss_db:find(gallery,[]),
     RecentImages = boss_db:find(image,[],15),
     {ok,[{images,RecentImages},{galleries,Galleries}]}.
+
+add('GET',[]) ->
+    Galleries = boss_db:find(gallery,[]),
+    {ok, [{galleries,Galleries}]};
+add('POST',[]) ->
+    Title = Req:post_param("title"),
+    Description = Req:post_param("description"),
+    Medium = Req:post_param("medium"),
+    [{uploaded_file,OrigFilename,TmpFile,Length}|_Rest] = Req:post_files(),
+    Extension = filename:extension(OrigFilename),
+    I = image:new(id,Title,slug,Description,created,Medium,"1d3964dfd1664247fef81c265dedd7a4c040eb3a",Extension),
+    {ok,SI1} = I:save(),
+    {redirect, "/"}.
