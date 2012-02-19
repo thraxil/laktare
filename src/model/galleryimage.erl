54586ec6793baca42656130bb0de6dc80b4f4a7d
Anders Pearson <anders@columbia.edu>
2012-02-18 22:35:11 -0500

    next/prev gallery image

--- src/model/galleryimage.erl
+++ src/model/galleryimage.erl
@@ -9,4 +9,35 @@ before_create() ->
 			 boss_db:count(galleryimage, 
 				       [gallery_id = GalleryId]) + 1),
     {ok, ModifiedRecord}.
-    
+
+get_absolute_url() ->
+    G = gallery(),
+    I = image(),
+    G:get_image_url() ++ "/" ++ I:slug().
+
+prev_image() ->
+    G = gallery(),
+    case boss_db:count(galleryimage,[id > Id, gallery_id = G:id()]) of 
+	0 ->
+	    false;
+	_ ->
+	    [P|_Rest] = boss_db:find(galleryimage,[id > Id, gallery_id = G:id()],all,0,id,str_descending),
+	    P
+    end.
+
+next_image() ->
+    G = gallery(),
+    case boss_db:count(galleryimage,[id < Id, gallery_id = G:id()]) of 
+	0 ->
+	    false;
+	_ ->
+	    [N|_Rest] = boss_db:find(galleryimage,[id < Id, gallery_id = G:id()],all,0,id,str_ascending),
+	    N
+    end.
+
+
+    % def has_other_galleries(self):
+    %     return self.other_galleries().count() > 0
+
+    % def other_galleries(self):
+    %     return self.image.galleryimage_set.all().exclude(gallery=self.gallery)
