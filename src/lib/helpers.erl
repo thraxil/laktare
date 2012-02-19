adbbaa8cc033bbc16a11d8170d8a3cba447b9819
Anders Pearson <anders@columbia.edu>
2012-02-18 19:08:58 -0500

    more than one test image

--- src/lib/helpers.erl
+++ src/lib/helpers.erl
@@ -6,6 +6,10 @@ populate_test_data() ->
     {ok,SG1} = G:save(),
     I = image:new(id,"first image",slug,"description",created,"medium","1d3964dfd1664247fef81c265dedd7a4c040eb3a",".jpg"),
     {ok,SI1} = I:save(),
+    I2 = image:new(id,"second image",slug,"description",created,"medium","477da14b4128da9e94677d5a61e786874484cd12",".jpg"),
+    {ok,SI2} = I2:save(),
     GI = galleryimage:new(id,SG1:id(),SI1:id(),ordinality),
     {ok,SGI1} = GI:save(),
-    {SG1,SI1,SGI1}.
+    GI2 = galleryimage:new(id,SG1:id(),SI2:id(),ordinality),
+    {ok,SGI2} = GI2:save(),
+    {SG1,SI1,SGI1,SI2,SGI2}.
