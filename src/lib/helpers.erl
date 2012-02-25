dd945de3da70db97513ce4ed97de7520ac15233f
Anders Pearson <anders@columbia.edu>
2012-02-25 18:36:41 -0500

    making test data have staggered timestamps

--- src/lib/helpers.erl
+++ src/lib/helpers.erl
@@ -4,15 +4,17 @@
 populate_test_data() ->
     G = gallery:new(id,"first gallery",slug,"description",ordinality),
     {ok,SG1} = G:save(),
-    I = image:new(id,"first image",slug,"description",{{2012,2,25},{23,20,9}},"medium","1d3964dfd1664247fef81c265dedd7a4c040eb3a",".jpg"),
+    I = image:new(id,"first image",slug,"description",created,"medium","1d3964dfd1664247fef81c265dedd7a4c040eb3a",".jpg"),
     {ok,SI1} = I:save(),
-    I2 = image:new(id,"second image",slug,"description",{{2012,2,25},{23,20,9}},"medium","477da14b4128da9e94677d5a61e786874484cd12",".jpg"),
+    SI3 = SI1:set(created_time,{{2012,2,25},{23,20,9}}),
+    {ok,SI4} = SI3:save(),
+    I2 = image:new(id,"second image",slug,"description",created_time,"medium","477da14b4128da9e94677d5a61e786874484cd12",".jpg"),
     {ok,SI2} = I2:save(),
-    GI = galleryimage:new(id,SG1:id(),SI1:id(),ordinality),
+    GI = galleryimage:new(id,SG1:id(),SI4:id(),ordinality),
     {ok,SGI1} = GI:save(),
     GI2 = galleryimage:new(id,SG1:id(),SI2:id(),ordinality),
     {ok,SGI2} = GI2:save(),
-    {SG1,SI1,SGI1,SI2,SGI2}.
+    {SG1,SI4,SGI1,SI2,SGI2}.
 
 % from: http://lethain.com/formatting-multipart-formdata-in-erlang/
 format_multipart_formdata(Boundary, Fields, Files) ->
