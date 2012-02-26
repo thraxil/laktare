ec52e31914d8f71e7b16ace59b544038f5d83e68
Anders Pearson <anders@columbia.edu>
2012-02-25 22:19:21 -0500

    load json dump

--- src/lib/helpers.erl
+++ src/lib/helpers.erl
@@ -47,3 +47,66 @@ head_or_false([]) ->
 head_or_false([H|_]) ->
     H.
 
+parse_datetime(DTString) ->
+    Fields = [list_to_integer(I) || I <- string:tokens(DTString,"{,}")],
+    {Year,Month,Day,Hour,Minute,Second} = list_to_tuple(Fields),
+    {{Year,Month,Day},{Hour,Minute,Second}}.
+
+load() ->
+    load_from_json("/home/anders/myopica.json").
+
+load_from_json(FilePath) ->
+    {ok,Data} = file:read_file(FilePath),
+    {struct,TopLevel} = mochijson:decode(Data),
+    {array,Images} = proplists:get_value("images",TopLevel),
+    {array,Galleries} = proplists:get_value("galleries",TopLevel),
+    {array,GalleryImages} = proplists:get_value("galleryimages",TopLevel),
+    
+    CreatedImages = lists:map(fun({struct,PList}) ->  
+				      Title = proplists:get_value("title",PList),
+				      Description = proplists:get_value("description",PList),
+				      Medium = proplists:get_value("medium",PList),
+				      Slug = proplists:get_value("slug",PList),
+				      AHash = proplists:get_value("ahash",PList),
+				      Extension = proplists:get_value("extension",PList),
+				      Created = parse_datetime(proplists:get_value("created",PList)),
+				      I = image:new(id,Title,Slug,Description,Created,Medium,AHash,Extension),
+				      {ok,SI} = I:save(),
+                                      % update the slug and created times
+				      I2 = SI:set([{slug,Slug},{created_time,Created}]),
+				      {ok,SI2} = I2:save(),
+				      SI2
+			      end, Images),
+    CreatedGalleries = lists:map(fun({struct,PList}) ->
+					 io:format("~p~n",[PList]),
+					 Title = proplists:get_value("title",PList),
+					 Slug = proplists:get_value("slug",PList),
+					 Description = proplists:get_value("description",PList),
+					 Ordinality = proplists:get_value("ordinality",PList),
+					 G = gallery:new(id,Title,Slug,Description,ordinality),
+					 {ok,SG} = G:save(),
+					 % update the ordinality
+					 G2 = SG:set(ordinality,Ordinality),
+					 {ok,SG2} = G2:save(),
+					 SG2
+					 end,
+				 Galleries),
+    CreatedGIs = lists:map(
+    		   fun({struct,PList}) ->
+    			   io:format("~p~n",[PList]),
+    			   ImageSlug = proplists:get_value("image",PList),
+    			   GallerySlug = proplists:get_value("gallery",PList),
+    			   Ordinality = proplists:get_value("ordinality",PList),
+    			   io:format("~p~n",[boss_db:count(image,[slug=ImageSlug])]),
+    			   io:format("~p~n",[boss_db:count(gallery,[slug=GallerySlug])]),
+    			   Image = hd(boss_db:find(image,[slug=ImageSlug])),
+    			   Gallery = hd(boss_db:find(gallery,[slug=GallerySlug])),
+    			   GI = galleryimage:new(id,Gallery:id(),Image:id(),ordinality),
+    			   {ok,SGI} = GI:save(),
+    			   GI2 = SGI:set(ordinality,Ordinality),
+    			   {ok,SGI2} = GI2:save(),
+    			   SGI2
+    			   end, GalleryImages),
+
+    {ok}.
+    
